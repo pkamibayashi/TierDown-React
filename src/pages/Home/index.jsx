22cd4707c908d4e8a9f13e3356c5d701d7d0660d
pkamibayashi <paulo_kamibayashi@hotmail.com>
2023-03-09 04:08:53 -0300

    home style done

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,31 +1,35 @@
-import { HomeIcon } from '@heroicons/react/24/solid';
-import { TypeAnimation } from 'react-type-animation';
-import { Link } from 'react-router-dom';
+import { HomeIcon } from "@heroicons/react/24/solid";
+import { TypeAnimation } from "react-type-animation";
+import { Link } from "react-router-dom";
 
 export function Home() {
   return (
-    <div className=" h-screen flex flex-row px-20  bg-gradient-to-r from-violet-500 to-fuchsia-500">
-      <div className="text-6xl flex flex-col items-center justify-center w-2/4 px-1 gap-y-20 space-y-40 p-10 md: hidden ">
-        <h1 className="italic text-white	font-semibold">
-          Bem vindo ao TearDown Tracker!{' '}
-        </h1>
-        <TypeAnimation
-          className="text-gray-300 italic drop-shadow-lg "
-          sequence={['Uma Ferramenta Lenovo.', 10000, '']}
-          speed={20}
-          wrapper="h2"
-          repeat={Infinity}
-        />
+    <div className="h-screen flex flex-row px-20  bg-gradient-to-r from-purple-500 to-pink-500 ">
+      <div className="text-6xl flex flex-col items-center justify-center w-2/4 px-8 gap-y-20 space-y-40 p-10">
+        <div className="drop-shadow-lg before:block before:absolute before:-inset-1 before:-skew-y-3 before:bg-pink-500 relative inline-block">
+          <h1 className=" text-slate-300 font-medium   drop-shadow-lg text-white	relative   md:block hidden">
+            Bem vindo ao TearDown Tracker!{" "}
+          </h1>
+        </div>
+        <div className="md:block hidden ">
+          <TypeAnimation
+            className="text-gray-300 italic drop-shadow-lg"
+            sequence={["Uma Ferramenta Lenovo.", 10000, ""]}
+            speed={20}
+            wrapper="h2"
+            repeat={Infinity}
+          />
+        </div>
       </div>
 
-      <div className="flex items-center justify-center w-2/4 flex-col gap-14 md:  flex justify-center	items-center">
+      <div className="flex items-center justify-center w-2/4 flex-col gap-14 md:flex mr-16">
         <Link to="/login">
-          <button className=" bg-indigo-500 px-4 py-8 w-80 text-3xl text-gray-300 italic rounded-lg drop-shadow-md hover:bg-indigo-600 font-semibold">
+          <button className="px-4 py-8 w-80 text-3xl text-gray-300 font-black italic rounded-lg drop-shadow-lg bg-gradient-to-r from-green-400 to-blue-500  hover:from-pink-500 hover:to-yellow-500 ... font-semibold">
             Fazer Login
           </button>
         </Link>
         <Link to="/signup">
-          <button className=" bg-indigo-500 px-4  py-8 w-80 text-3xl text-gray-300 italic rounded-lg drop-shadow-md hover:bg-indigo-600 font-semibold">
+          <button className="px-4 py-8 w-80 text-3xl text-gray-300 font-black italic rounded-lg drop-shadow-lg d bg-gradient-to-r from-green-400 to-blue-500  hover:from-pink-500 hover:to-yellow-500 ... font-semibold">
             Criar uma conta
           </button>
         </Link>
--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -33,31 +33,37 @@ export function Login() {
   }
 
   return (
-    <form onSubmit={handleSumit}>
-      <label>Email:</label>
-      <input
-        type="email"
-        name="email"
-        value={form.email}
-        onChange={handleChange}
-      />
-      <label>Senha:</label>
-      <input
-        type="password"
-        name="password"
-        value={form.password}
-        onChange={handleChange}
-      />
+    <div className="h-screen flex flex-row bg-gradient-to-r from-purple-500 to-pink-500">
+      <div className="flex justify-center w-4/12 h-96 bg-white rounded-xl shadow-xl mx-auto my-auto  p-10 ">
+        <form
+          onSubmit={handleSumit}
+          className="flex flex-col items-center justify-center"
+        >
+          <label className="text-gray-300 font-medium">Email:</label>
+          <input
+            type="email"
+            name="email"
+            value={form.email}
+            onChange={handleChange}
+            className="border rounded-md px-2 py-1 mb-4 focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent"
+          />
+          <label className="text-gray-300 font-medium">Senha:</label>
+          <input
+            type="password"
+            name="password"
+            value={form.password}
+            onChange={handleChange}
+            className="border rounded-md px-2 py-1 mb-4 focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent"
+          />
 
-      <button
-        type="submit"
-        className="rounded-md bg-indigo-600 py-2 px-3 text-sm font-semibold
-      text-white shadow-sm hover:bg-indigo-500 focus-visible:outline
-      focus-visible:outline-2 focus-visible:outline-offset-2
-      focus-visible:outline-indigo-600"
-      >
-        Entrar!
-      </button>
-    </form>
+          <button
+            type="submit"
+            className="bg-gradient-to-r from-green-400 to-blue-500 hover:from-pink-500 hover:to-yellow-500 text-white font-semibold py-2 px-4 rounded-md shadow-md focus:outline-none focus:ring-2 focus:ring-purple-600 focus:ring-opacity-50"
+          >
+            Entrar!
+          </button>
+        </form>
+      </div>
+    </div>
   );
 }
