1e79e39be13fc370fac77548e81cbc3766592fdd
Paulo Kamibayashi <108964469+pkamibayashi@users.noreply.github.com>
2023-03-09 18:23:52 -0300

    changed prodct link

--- src/api/api.js
+++ src/api/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const apiURLs = {
   development: "http://localhost:4000",
-  production: "https://teardownapi.cyclic.app",
+  production: "https://real-lime-magpie-tutu.cyclic.app",
 };
 
 const api = axios.create({ baseURL: apiURLs[process.env.NODE_ENV] });
