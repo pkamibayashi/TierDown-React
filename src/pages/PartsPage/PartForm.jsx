7e4d662554724f3842bc515fc062dafc846e5609
pkamibayashi <paulo_kamibayashi@hotmail.com>
2023-03-09 15:57:06 -0300

    PartPage estilizada, PartForm Estilizado, NavBar incluida

--- src/pages/PartsPage/PartForm.jsx
+++ src/pages/PartsPage/PartForm.jsx
@@ -1,7 +1,10 @@
 import { api } from "../../api/api";
 import { useState } from "react";
-
+import { useNavigate } from "react-router-dom";
+import { FaPlus, FaUser } from "react-icons/fa";
+import { Link } from "react-router-dom";
 function PartForm() {
+  const navigate = useNavigate(); // movido para dentro do componente
   const [newPart, setNewPart] = useState({
     partNumber: "",
     partName: "",
@@ -15,6 +18,7 @@ function PartForm() {
 
   async function handleFormSubmit(event) {
     event.preventDefault();
+
     try {
       const response = await api.post("part/createPart", newPart);
       console.log("Part created:", response.data);
@@ -24,39 +28,109 @@ function PartForm() {
         partName: "",
         machineSerialNumber: "",
       });
+      navigate("/parts");
     } catch (error) {
       console.error(error);
     }
   }
 
   return (
-    <form onSubmit={handleFormSubmit}>
-      <input
-        type="text"
-        name="partNumber"
-        placeholder="Part number"
-        value={newPart.partNumber}
-        onChange={handleInputChange}
-        required
-      />
-      <input
-        type="text"
-        name="partName"
-        placeholder="Part name"
-        value={newPart.partName}
-        onChange={handleInputChange}
-        required
-      />
-      <input
-        type="text"
-        name="machineSerialNumber"
-        placeholder="Machine serial number"
-        value={newPart.machineSerialNumber}
-        onChange={handleInputChange}
-        required
-      />
-      <button type="submit">Create Part</button>
-    </form>
+    <div className="h-screen flex flex-col">
+      <nav className="bg-gray-800 text-white h-16">
+        <div className="container mx-auto px-4 h-12">
+          <div className="flex items-center justify-between py-2">
+            <div>
+              <Link to="/" className="text-lg font-semibold">
+                My App
+              </Link>
+            </div>
+            <div className="flex items-center">
+              <Link
+                to="/partform"
+                className="flex items-center px-4 py-2 rounded-md hover:bg-gray-700"
+              >
+                <FaPlus className="mr-2" />
+                Adicionar Part
+              </Link>
+              <Link
+                to="/profile"
+                className="flex items-center px-4 py-2 rounded-md hover:bg-gray-700"
+              >
+                <FaUser className="mr-2" />
+                Perfil
+              </Link>
+            </div>
+          </div>
+        </div>
+      </nav>
+
+      <div className="flex flex-col items-center justify-center bg-gray-100 flex-1">
+        <div className="w-full max-w-2xl bg-white rounded-xl shadow-lg p-10">
+          <h2 className="text-xl font-semibold mb-6">Create a New Part</h2>
+          <form onSubmit={handleFormSubmit}>
+            <div className="mb-4">
+              <label
+                className="block text-gray-700 text-sm font-bold mb-2"
+                htmlFor="partNumber"
+              >
+                Part Number:
+              </label>
+              <input
+                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                type="text"
+                name="partNumber"
+                placeholder="Part number"
+                value={newPart.partNumber}
+                onChange={handleInputChange}
+                required
+              />
+            </div>
+            <div className="mb-4">
+              <label
+                className="block text-gray-700 text-sm font-bold mb-2"
+                htmlFor="partName"
+              >
+                Part Name:
+              </label>
+              <input
+                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                type="text"
+                name="partName"
+                placeholder="Part name"
+                value={newPart.partName}
+                onChange={handleInputChange}
+                required
+              />
+            </div>
+            <div className="mb-4">
+              <label
+                className="block text-gray-700 text-sm font-bold mb-2"
+                htmlFor="machineSerialNumber"
+              >
+                Machine Serial Number:
+              </label>
+              <input
+                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                type="text"
+                name="machineSerialNumber"
+                placeholder="Machine serial number"
+                value={newPart.machineSerialNumber}
+                onChange={handleInputChange}
+                required
+              />
+            </div>
+            <div className="flex items-center justify-center">
+              <button
+                className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+                type="submit"
+              >
+                Create Part
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
   );
 }
 
--- src/pages/PartsPage/PartsPage.jsx
+++ src/pages/PartsPage/PartsPage.jsx
@@ -1,5 +1,7 @@
 import { api } from "../../api/api";
 import { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import { FaPlus, FaUser } from "react-icons/fa";
 
 function PartPage() {
   const [parts, setParts] = useState([]);
@@ -33,19 +35,46 @@ function PartPage() {
 
   // renderiza a lista de parts em cartões
   const partCards = parts.map((part) => (
-    <div className="card" key={part._id}>
-      <div className="card-body">
-        <h5 className="card-title">{part.partNumber}</h5>
-        <h6 className="card-subtitle mb-2 text-muted">{part.partName}</h6>
-        <p className="card-text">{part.machineSerialNumber}</p>
-        <p className="card-text">{part.userCreator.name}</p>
-        <p className="card-text">{part.createdAt}</p>
+    <div
+      className="max-w-md mx-auto rounded-xl shadow-lg overflow-hidden my-5 border-2 border-gray-200"
+      key={part._id}
+    >
+      <div className="px-4 py-5 sm:p-6">
+        <h3 className="text-lg leading-6 font-medium text-gray-900">
+          {part.partNumber}
+        </h3>
+        <p className="mt-1 text-sm text-gray-500">
+          {part.partName} - {part.machineSerialNumber}
+        </p>
+        <div className="flex items-center mt-2 text-sm text-gray-500">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-4 w-4 mr-1"
+            viewBox="0 0 20 20"
+            fill="currentColor"
+          >
+            <path
+              fill-rule="evenodd"
+              d="M10 2a8 8 0 100 16 8 8 0 000-16zm0 14a6 6 0 110-12 6 6 0 010 12z"
+              clip-rule="evenodd"
+            />
+            <path
+              fill-rule="evenodd"
+              d="M10 11a2 2 0 100-4 2 2 0 000 4z"
+              clip-rule="evenodd"
+            />
+          </svg>
+          {part.userCreator.name}
+        </div>
+        <p className="mt-2 text-sm text-gray-500">
+          Created At: {part.createdAt.slice(0, 10)}
+        </p>
         {localStorage.getItem("loggedInUser") &&
         JSON.parse(localStorage.getItem("loggedInUser")).user.role ===
           "ADMIN" ? (
           <button
             type="button"
-            className="btn btn-danger"
+            className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 mt-4 rounded"
             onClick={() => deletePart(part._id)}
           >
             Delete
@@ -56,8 +85,45 @@ function PartPage() {
   ));
 
   return (
-    <div className="container">
-      <div>{partCards}</div>
+    <div className="h-screen flex flex-col">
+      {/* Navbar */}
+      <nav className="bg-gray-800 text-white h-16">
+        <div className="container mx-auto px-4 h-12">
+          <div className="flex items-center justify-between py-2">
+            <div>
+              <Link to="/" className="text-lg font-semibold">
+                My App
+              </Link>
+            </div>
+            <div className="flex items-center">
+              <Link
+                to="/partform"
+                className="flex items-center px-4 py-2 rounded-md hover:bg-gray-700"
+              >
+                <FaPlus className="mr-2" />
+                Adicionar Part
+              </Link>
+              <Link
+                to="/profile"
+                className="flex items-center px-4 py-2 rounded-md hover:bg-gray-700"
+              >
+                <FaUser className="mr-2" />
+                Perfil
+              </Link>
+            </div>
+          </div>
+        </div>
+      </nav>
+
+      {/* Conteúdo da página */}
+      <div className="flex flex-col items-center justify-center bg-gray-100 flex-1">
+        <div className="w-full max-w-2xl bg-white rounded-xl shadow-lg p-10">
+          <h2 className="text-xl font-semibold mb-6">List of Parts</h2>
+          <div className="flex flex-col items-center justify-center">
+            {partCards}
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
